388c9bc1389c8ad4060f9ad8b86e5981cf4db70b
Nico-Castelo <ncastelolourido@gmail.com>
2026-07-16 14:41:07 +0200

    first commit

---
+++ Source/Blade/BladeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BladeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLADE_API ABladeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
